833f25760f8f15952f36ba63d66c32374ab5b731
Divneet Sethi <divneet91@yahoo.com>
2015-02-22 18:28:42 -0500

    comments added

--- DivneetSethi_Assignment2/Scripts/Mobile_JavaScript.js
+++ DivneetSethi_Assignment2/Scripts/Mobile_JavaScript.js
@@ -1,3 +1,8 @@
-﻿function init(){
+﻿/*File: Mobile_JavaScript.js 
+Author: Divneet Singh Sethi 
+Website: divneetsethi.github.com/mobile
+Desription: javascript for site */
+
+function init() {
     console.log("Mobile app started.");
 }
